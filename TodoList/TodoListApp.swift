6f2431d1b9d37b023100419c2a29e34fa5cb2679
jgestradam <jgestradam@unal.edu.co>
2022-11-18 15:53:10 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Joseph Estrada on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
