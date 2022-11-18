761f58a567df3d165d63007eec034c5d86ff41c3
jgestradam <jgestradam@unal.edu.co>
2022-11-18 16:30:50 -0500

    Part1.CreateAListOfTodoItems

---
+++ TodoList/Views/AddView.swift
@@ -0,0 +1,47 @@
+//
+//  AddView.swift
+//  TodoList
+//
+//  Created by Joseph Estrada on 11/18/22.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Save".uppercased())
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                })
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Item ")
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddView()
+        }
+    }
+}
