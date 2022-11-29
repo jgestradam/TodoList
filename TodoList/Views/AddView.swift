c7a259d51e37754b8e4d40a2fe22eb2c0c150420
jgestradam <jgestradam@unal.edu.co>
2022-11-29 16:40:29 -0500

    Part3.AddAViewModelWith@EnvironmentObject

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -9,40 +9,66 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alerTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
             VStack {
                 TextField("Type something here...", text: $textFieldText)
+                    .fontWeight(.semibold)
                     .padding(.horizontal)
                     .frame(height: 55)
-                    .background(Color.gray)
-                    .opacity(0.5)
+                    .background(.thinMaterial)
                     .cornerRadius(10)
                 
-                Button(action: {
-                    
-                }, label: {
+                Button {
+                    saveButtonPressed()
+                } label: {
                     Text("Save".uppercased())
-                        .foregroundColor(.white)
                         .font(.headline)
-                        .frame(height: 55)
                         .frame(maxWidth: .infinity)
-                        .background(Color.accentColor)
-                        .cornerRadius(10)
-                })
+                        .frame(height: 40)
+                }
+                .buttonStyle(.borderedProminent)
             }
             .padding(14)
         }
-        .navigationTitle("Add an Item ")
+        .navigationTitle("Add an Item 🖊️")
+        .alert(isPresented: $showAlert, content: getAlert)
     }
+    
+    func saveButtonPressed() {
+        if textIsAppropiate() {
+            listViewModel.addItem(title: textFieldText)
+            dismiss()
+        }
+    }
+    
+    func textIsAppropiate() -> Bool {
+        if textFieldText.count < 3 {
+            alerTitle = "Your new todo item must be at least 3 characters 😨"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alerTitle))
+    }
+    
 }
 
 struct AddView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
+        NavigationStack {
             AddView()
         }
+        .environmentObject(ListViewModel())
     }
 }
